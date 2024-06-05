f2af439e1d331b2d839b932341bd1bf2a0754db3
marcosdotps <marcosdotps@github.com>
2024-06-05 11:11:15 +0200

    precofigure locales

--- install.sh
+++ install.sh
@@ -18,10 +18,26 @@ setup-lvim() {
 
 apt-install() {
     echo "==> Start apt-installing packages"
-    sudo apt update
+    apt update
+   
+   # Preconfigure the locale settings to avoid interactive prompts
+    locale-gen en_US.UTF-8
+    echo 'LANG="en_US.UTF-8"' | sudo tee /etc/default/locale
+    echo 'LANGUAGE="en_US:en"' | sudo tee -a /etc/default/locale
+    echo 'LC_ALL="en_US.UTF-8"' | sudo tee -a /etc/default/locale
+
+    # Apply the locale settings
+    sudo update-locale LANG=en_US.UTF-8 LANGUAGE=en_US:en LC_ALL=en_US.UTF-8
+
+    # Export locale variables for the current session
+    export LANG=en_US.UTF-8
+    export LANGUAGE=en_US:en
+    export LC_ALL=en_US.UTF-8
+
+    export LANG=en_US.UTF-8
+    export LANGUAGE=en_US
+    export LC_ALL=en_US.UTF-8
     sudo apt install -y exuberant-ctags bat tree shellcheck icdiff autojump jq ripgrep libevent-dev ncurses-dev build-essential bison pkg-config
-    sudo locale-gen en_US.UTF-8
-    sudo dpkg-reconfigure locales
     echo "==> Finished apt-installing packages"
 }
 
